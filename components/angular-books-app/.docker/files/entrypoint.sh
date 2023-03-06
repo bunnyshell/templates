425c2d5bb4ff9192df448f962ac20a3d89048b7a
Aris Buzachis <aris.buzachis@bunnyshell.com>
2023-03-06 12:06:57 +0200

    fix: tag updates and angular-books-app prod entrypoint
    Signed-off-by: Aris Buzachis <aris.buzachis@bunnyshell.com>

--- components/angular-books-app/.docker/files/entrypoint.sh
+++ components/angular-books-app/.docker/files/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-if [ -n "$API_URL" ]; then
-    echo "Replacing API_URL..."
+# replacement for the dev environment / target
+if [ -n "$API_URL" ] && [ -f 'src/environments/environment.ts' ]; then
+    echo "Replacing API_URL for dev..."
 
     sed -i "s|apiUrl: '.*',|apiUrl: '${API_URL}',|" src/environments/environment.ts
+fi
+
+# replacement for the prod environment / target
+if [ -n "$API_URL" ] && [ -d '/usr/share/nginx/html' ]; then
+    echo "Replacing API_URL for prod..."
+
+    find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|this.apiUrl=\"[^\"]*\"|this.apiUrl=\"${API_URL}\"|" {} \;
 fi
 
 exec "$@"
