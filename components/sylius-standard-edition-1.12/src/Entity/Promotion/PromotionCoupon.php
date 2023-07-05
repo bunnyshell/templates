3c40fab51dad0855d8a5c25d94ff81ca9124f959
Sorin Dumitrescu <sorin.dumitrescu@bunnyshell.com>
2023-07-05 10:15:05 +0300

    Added Sylius templates

---
+++ components/sylius-standard-edition-1.12/src/Entity/Promotion/PromotionCoupon.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Promotion;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\PromotionCoupon as BasePromotionCoupon;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="sylius_promotion_coupon")
+ */
+#[ORM\Entity]
+#[ORM\Table(name: 'sylius_promotion_coupon')]
+class PromotionCoupon extends BasePromotionCoupon
+{
+}
